bc065bc48220cc70a0735b85b00c8a1a6ed07745
Aman Utemuratov <aman.utemuratov@gmail.com>
2016-11-01 23:34:27 +0600

    test commit

--- src/main/java/dataStructure/SparseTable.java
+++ src/main/java/dataStructure/SparseTable.java
@@ -4,4 +4,44 @@ package dataStructure;
  * Created by Aman on 11/1/2016.
  */
 public class SparseTable {
+
+    private int sparseTable[][];
+    private int logPowerOfTwo[];
+
+    public SparseTable(int arr[]) {
+        int n = arr.length;
+        logPowerOfTwo = new int[n + 1];
+        buildLogTable(arr);
+
+        sparseTable = new int[logPowerOfTwo[n] + 1][n];
+        buildSparseTable(arr);
+    }
+
+    private void buildLogTable(int[] arr) {
+        for (int i = 2; i <= arr.length; i++) {
+            logPowerOfTwo[i] = logPowerOfTwo[i >> 1] + 1;
+        }
+    }
+
+    private void buildSparseTable(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            sparseTable[0][i] = arr[i];
+        }
+
+        for (int k = 1; (1 << k) < n; k++) {
+            for (int j = 0; j + (1 << k) <= n; j++) {
+                int x = sparseTable[k - 1][j];
+                int y = sparseTable[k - 1][j + (1 << k) - 1];
+                sparseTable[k][j] = Math.min(x, y);
+            }
+        }
+    }
+
+    public int getMinInRange(int left, int right) {
+        int dif = logPowerOfTwo[right - left];
+        int x = sparseTable[dif][left];
+        int y = sparseTable[dif][right - (1 << dif) + 1];
+        return Math.min(x, y);
+    }
 }
--- src/test/java/dataStructure/SparseTableTest.java
+++ src/test/java/dataStructure/SparseTableTest.java
@@ -1,5 +1,6 @@
 package dataStructure;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,9 +9,63 @@ import static org.junit.Assert.*;
  * Created by Aman on 11/1/2016.
  */
 public class SparseTableTest {
-    @Test
-    public void getMinInRange() throws Exception {
 
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    SparseTable sparseTable;
+
+    @Before
+    public void tearDown() {
+        sparseTable = new SparseTable(arr);
+    }
+
+    @Test
+    public void getMinInRangeFull() throws Exception {
+        //given
+        int left = 0;
+        int right = arr.length;
+        int expected = getExpectedMinimum(left, right);
+
+        //when
+        int actual = sparseTable.getMinInRange(left, right);
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    private int getExpectedMinimum(int left, int right) {
+        int expected = Integer.MAX_VALUE;
+        for (int i = left; i < right; i++) {
+            expected = Math.min(expected, arr[i]);
+        }
+        return expected;
+    }
+
+    @Test
+    public void getMinInRangeZeroToThree() throws Exception {
+        //given
+        int left = 0;
+        int right = 3;
+        int expected = getExpectedMinimum(left, right);
+
+        //when
+        int actual = sparseTable.getMinInRange(left, right);
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getMinInRangePoint() throws Exception {
+        //given
+        int left = 3;
+        int right = 4;
+        int expected = getExpectedMinimum(left, right);
+
+        //when
+        int actual = sparseTable.getMinInRange(left, right);
+
+        //then
+        assertEquals(expected, actual);
     }
 
 }
