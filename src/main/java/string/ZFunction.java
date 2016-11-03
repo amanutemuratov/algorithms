f48a6885e2b1fb4f0053b4ae81ed79e7d359f8cd
Aman Utemuratov <aman.utemuratov@gmail.com>
2016-11-03 18:49:56 +0600

    bug fix Z-function

--- src/main/java/string/ZFunction.java
+++ src/main/java/string/ZFunction.java
@@ -19,13 +19,13 @@ public class ZFunction {
         int rightPosition = 0;
 
         for (int i = 1; i < n; i++) {
-            if (i < rightPosition) {
+            if (i <= rightPosition) {
                 z[i] = Math.min(z[i - leftPosition], rightPosition - i + 1);
             }
             while (i + z[i] < n && arr[z[i]] == arr[i + z[i]]) {
                 z[i]++;
             }
-            if (z[i] > rightPosition) {
+            if (i+z[i]-1 > rightPosition) {
                 leftPosition = i;
                 rightPosition = i + z[i] - 1;
             }
